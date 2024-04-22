336d33d59b4c8aa7716aeee72ed48ad158096d6c
Vishesh-Dhiman <152509757+Vishesh-Dhiman@users.noreply.github.com>
2024-04-22 18:45:01 +0530

    Update progress.py

--- main/plugins/progress.py
+++ main/plugins/progress.py
@@ -52,7 +52,7 @@ async def progress_for_pyrogram(
             round(percentage, 2),
         )
 
-        tmp = progress + "**📈GROSSS: {0} of {1}\n\n🚀Speed: {2}/s\n\n🧲ETA: {3}\n\n🤖ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ [Cʜᴏᴛᴜ](https://t.me/PiroChotu)**".format(
+        tmp = progress + "**📈GROSSS: {0} of {1}\n\n🚀Speed: {2}/s\n\n🧲ETA: {3}\n\n🤖ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ [Cʜᴏᴛᴜ](https://t.me/CyberChotu)**".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
